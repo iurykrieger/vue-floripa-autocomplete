672aa031844864f7ac35cdf4a56a7b7879a9e238
Iury Krieger <iurykrieger96@gmail.com>
2019-05-25 10:32:02 -0300

    storybook addons

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,25 @@
+import { storiesOf } from '@storybook/vue'
+import { withNotes } from '@storybook/addon-notes'
+import { text, withKnobs } from '@storybook/addon-knobs'
+import VueFloripaButton from './Button.vue'
+import notes from './Button.md'
+
+storiesOf('Button', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withNotes)
+  .add('Default', () => ({
+    components: { VueFloripaButton },
+    props: {
+      text: {
+        type: String,
+        default: text('text', 'Vue Floripa!')
+      }
+    },
+    template: `
+      <vue-floripa-button
+        :text="text"
+      />
+    `
+  }), {
+    notes
+  })
